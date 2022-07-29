03a504e89212d4e40e2dade9b1fc639d88699022
Abhishek <abhishekkr.smm@gmail.com>
2022-07-29 16:44:41 +0530

    Added board state and also react events

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sqaure from './Sqaure';
-//? <div is the row>
+
+//? we pass the board index in the value of the sqaure.
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleSqaureClick = position => {
+    if (board[position]) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : '0';
+        }
+
+        return square;
+      });
+    });
+    setIsXNext(prev => !prev);
+  };
+
+  const renderSqaure = position => {
+    return (
+      <Sqaure
+        value={board[position]}
+        onClick={() => {
+          handleSqaureClick(position);
+        }}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Sqaure value={1} />
-        <Sqaure value={2} />
-        <Sqaure value={3} />
+        {renderSqaure(0)}
+        {renderSqaure(1)}
+        {renderSqaure(2)}
       </div>
       <div className="board-row">
-        <Sqaure value={4} />
-        <Sqaure value={5} />
-        <Sqaure value={6} />
+        {renderSqaure(3)}
+        {renderSqaure(4)}
+        {renderSqaure(5)}
       </div>
       <div className="board-row">
-        <Sqaure value={7} />
-        <Sqaure value={8} />
-        <Sqaure value={9} />
+        {renderSqaure(6)}
+        {renderSqaure(7)}
+        {renderSqaure(8)}
       </div>
     </div>
   );
--- src/Board/Sqaure.jsx
+++ src/Board/Sqaure.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 //Here we used object destructuring to remove props
 
-const Sqaure = ({ value }) => {
+const Sqaure = ({ value, onClick }) => {
   // console.log(value);
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
